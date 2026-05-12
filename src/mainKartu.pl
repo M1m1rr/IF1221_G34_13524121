72a08e74a49adcc26c34dedf879c240f60f0dd3b
ausahmya-gif <ausahmya@gmail.com>
2026-05-12 21:40:50 +0700

    expanded function for mainKartu

--- src/mainKartu.pl
+++ src/mainKartu.pl
@@ -1,18 +1,39 @@
 
+putar_list(Input, Hasil) :-
+    rekursi_putar(Input, [], Hasil).
+rekursi_putar([], Accumulator, Accumulator).
+rekursi_putar([H|T], Accumulator, Hasil) :-
+    rekursi_putar(T, [H|Accumulator], Hasil).
 cocok(kartu(Warna, _), kartu(Warna, _)) :- !.
 cocok(kartu(_, Sama), kartu(_, Sama)):- !.
 cocok(kartu(hitam, _), kartu(_, _)):-!.
 efek(kartu,(_, skip)):-
-write('pemain berikutnya kehilangan giliran'), !.
+write('pemain berikutnya kehilangan giliran'), 
+nextturn,
+nextturn,!.
 efek(kartu(_, draw_two)):-
-write('pemain berikutnya terkena draw 2'), !.
+write('pemain berikutnya terkena draw 2'), 
+nextturn,
+ambilKartu,
+ambilKartu,
+nextturn,!.
 efek(kartu(_,draw_four)):-
-write('pemain berikutnya terkena draw 4'), !.
+write('pemain berikutnya terkena draw 4'),
+ambilKartu,
+ambilKartu,
+ambilKartu,
+ambilKartu,
+nextturn, !.
 efek(kartu(_,reverse)):-
-write('order pemain terbalik'), !.
-efek(_):-
-.
+write('order pemain terbalik'), 
+putar_list(DaftarAcak, DaftarAcak1),
+retract(urutan(DaftarAcak)),
+assertz(urutan(DaftarAcak1)).
+nextturn,!.
 
+efek(_):-
+nextturn,!
+.
 mainkanKartu(Index) :-
 giliran(Pemain),
 efek_aktif(Efek),
@@ -26,8 +47,8 @@ assertz(tangan(Pemain, Indekskartu1)),
 retract(draw_pile(Kartumeja)),
 assertz(draw_pile(Kartupemain)),
 format('~w memainkan kartu: ~w', [Pemain], [Kartupemain]),
-efek(Kartupemain),
-nextturn.
+urutan(DaftarAcak),
+efek(Kartupemain).
 
 
 
