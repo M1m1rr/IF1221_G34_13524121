3e49958212cd6f83c6489fe0d12b31d72d506302
ausahmya-gif <ausahmya@gmail.com>
2026-05-30 11:58:58 +0700

    fixed CekINfo little UI BUG and fixed palyer able to use mainkartu while getting efek draw 2 or 4

--- src/mainKartu.pl
+++ src/mainKartu.pl
@@ -87,6 +87,10 @@ efek(kartu(_, mimic)):-
 efek(_):-
     giliran_berikutnya,!
 .
+mainkanKartu(_) :-
+    efek_aktif(Efek),
+    (Efek== draw_two ; Efek == wild_draw_four),
+    format('woi Lu kena efek ~w harus ambil kartu gabisa mainkan kartu~n', [Efek]), !.
 mainkanKartu(Index) :-
     urutan_pemain([Pemain|_]),
     simpan_kartu_pemain(Pemain, Indekskartu),
